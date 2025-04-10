f9c87d1ceb0662c1ac64a21c1aac18cec9813311
mongsam2 <songjaehyun121476@gmail.com>
2025-04-10 16:45:29 +0900

    feat: Working Condition Entity 추가

--- src/main/java/butakcare/demo/domain/WorkingCondition.java
+++ src/main/java/butakcare/demo/domain/WorkingCondition.java
@@ -1,15 +1,23 @@
 package butakcare.demo.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 public class WorkingCondition {
 
+    @Id @GeneratedValue
+    private Long id;
+
     private Integer wage;
 
     @ManyToOne
     private Senior senior;
+
+    @OneToMany(mappedBy = "workingCondition")
+    private List<WorkingConditionTime> workingConditionTimeList = new ArrayList<>();
 }
--- src/main/java/butakcare/demo/domain/WorkingConditionTime.java
+++ src/main/java/butakcare/demo/domain/WorkingConditionTime.java
@@ -1,4 +1,26 @@
 package butakcare.demo.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
 public class WorkingConditionTime {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String day;
+
+    private LocalTime startTime;
+
+    private LocalTime endTime;
+
+    @ManyToOne
+    private WorkingCondition workingCondition;
 }
