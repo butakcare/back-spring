b2b42f8d220b8bbab68708f89b4101c910e633bc
mongsam2 <songjaehyun121476@gmail.com>
2025-04-10 10:51:48 +0900

    feat: Center Entitiy 정의

--- src/main/java/butakcare/demo/domain/Center.java
+++ src/main/java/butakcare/demo/domain/Center.java
@@ -1,4 +1,34 @@
 package butakcare.demo.domain;
 
+import butakcare.demo.common.Address;
+import butakcare.demo.common.Rank;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
 public class Center {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private String brn; //사업자 번호
+
+    private String siteUrl;
+
+    private String phoneNumber;
+
+    private Boolean bathingVehicle;
+
+    private Address address;
+
+    private Rank rank;
+
+    private LocalDate startDate;
 }
