9e95f64b4b04365fa38cb1df5ea663d6d010190a
mongsam2 <songjaehyun121476@gmail.com>
2025-04-11 13:00:54 +0900

    feat: Matching Entity

--- src/main/java/butakcare/demo/domain/Matching.java
+++ src/main/java/butakcare/demo/domain/Matching.java
@@ -1,4 +1,24 @@
 package butakcare.demo.domain;
 
+import butakcare.demo.common.MatchingStatus;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Matching {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private LocalDateTime createdDate;
+
+    @Enumerated(EnumType.STRING)
+    private MatchingStatus status;
+
+    @ManyToOne
+    private CaregiverCondition caregiverCondition;
+
+    @ManyToOne
+    private WorkingCondition workingCondition;
 }
