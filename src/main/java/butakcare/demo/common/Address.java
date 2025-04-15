8d3ed844dc1ff1affb8f9e9e9e330d06f1ac49ab
mongsam2 <songjaehyun121476@gmail.com>
2025-04-15 13:20:25 +0900

    fix: Address embedded 타입 기본 생성자 추가

--- src/main/java/butakcare/demo/common/Address.java
+++ src/main/java/butakcare/demo/common/Address.java
@@ -2,15 +2,12 @@ package butakcare.demo.common;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Embeddable
+@NoArgsConstructor
 public class Address {
     private String address;
     private String addressDetail;
-
-    public Address(String address, String addressDetail) {
-        this.address = address;
-        this.addressDetail = addressDetail;
-    }
 }
